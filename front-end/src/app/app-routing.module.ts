1b77ce78057e2d45a2d5ee7e0c54955c219e194d
unknown <santhoshyavd@gmail.com>
2022-08-04 18:11:32 +0530

    changes

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+import { ProductmasterComponent } from './productmaster/productmaster.component';
 import { AuthGuard } from './auth.guard';
 import { BformComponent } from './components/bform/bform.component';
 import { HomeComponent } from './home/home.component';
-import { MenuComponent } from './menu/menu.component';
-import { ProfileComponent } from './profile/profile.component';
 import { SignupComponent } from './signup/signup.component';
 const routes: Routes  =[  
    { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component:BformComponent},
   { path: 'home', component:HomeComponent,canActivate:[AuthGuard] },
   {path:'signup',component:SignupComponent},
-  {path :'menu',component:MenuComponent},
-  {path:'profile',component:ProfileComponent}
+  {path:'addproduct',component:ProductmasterComponent}
 ];
 
 @NgModule({
