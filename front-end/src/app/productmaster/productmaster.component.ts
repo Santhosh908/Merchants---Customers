c147738536cde6316706ab3c41095bbf3e4aae0f
unknown <santhoshyavd@gmail.com>
2022-08-07 22:55:39 +0530

    changes

--- front-end/src/app/productmaster/productmaster.component.ts
+++ front-end/src/app/productmaster/productmaster.component.ts
@@ -1,13 +1,51 @@
+
+
+
+// import {Component} from '@angular/core';
+
+// export interface PeriodicElement {
+//   name: string;
+//   position: number;
+//   weight: number;
+//   symbol: string;
+// }
+
+// const ELEMENT_DATA: PeriodicElement[] = [
+//   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+//   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+//   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+//   {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+//   {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+//   {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+//   {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+//   {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+//   {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+//   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+// ];
+
+// /**
+//  * @title Basic use of `<table mat-table>`
+//  */
+// @Component({
+//   selector: 'app-productmaster',
+//   templateUrl: './productmaster.component.html',
+//   styleUrls: ['./productmaster.component.css']
+// })
+// export class  ProductmasterComponent {
+//   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+//   dataSource = ELEMENT_DATA;
+// }
+
+
+
+
+
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog,MatDialogClose,MatDialogContent } from '@angular/material/dialog';
 import { range } from 'rxjs';
 import { DialogproductComponent } from '../dialogproduct/dialogproduct.component';
-// export interface tablecontent{
-//   position:number;
-//   pname:string;
-//   pid:string;
-// }
+
 
 @Component({
   selector: 'app-productmaster',
@@ -17,11 +55,12 @@ import { DialogproductComponent } from '../dialogproduct/dialogproduct.component
 
 export class ProductmasterComponent implements OnInit {
   a:any=[];
+  b:any=[];
   constructor(private dialog:MatDialog,private http:HttpClient) { }
   popup(){
     this.dialog.open(DialogproductComponent)
   }
-  
+  displayedColumns: string[] = ['productid', 'productname']
   showtable(){
     console.log("HI")
     this.http.get("http://localhost:3000/showtables").subscribe((data)=>{
@@ -31,10 +70,14 @@ export class ProductmasterComponent implements OnInit {
       for(let i=0;i<l;i++){
         this.a.push({position:i,pid:key[i].productid,pname:key[i].productname})
       }
-      console.log(this.a)
-      // for(key in data){
-        // console.log(JSON.stringify(data)[1]);
+      console.log(this.a[0])
+      // var key=Object.values(data)
+      // var l=key.length;
+      // for(let i=0;i<l;i++){
+      //   console.log(key[i])
       // }
+      // this.a=key
+      // console.log(this.a)
     })
   }
   ngOnInit(): void {
