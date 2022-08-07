3ecc8eb80d1f446bc0e2790fe715e7db46f7d4c5
unknown <santhoshyavd@gmail.com>
2022-08-07 21:17:57 +0530

    changes

--- front-end/src/app/productmaster/productmaster.component.ts
+++ front-end/src/app/productmaster/productmaster.component.ts
@@ -3,29 +3,33 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog,MatDialogClose,MatDialogContent } from '@angular/material/dialog';
 import { range } from 'rxjs';
 import { DialogproductComponent } from '../dialogproduct/dialogproduct.component';
+// export interface tablecontent{
+//   position:number;
+//   pname:string;
+//   pid:string;
+// }
+
 @Component({
   selector: 'app-productmaster',
   templateUrl: './productmaster.component.html',
   styleUrls: ['./productmaster.component.css']
 })
-export class ProductmasterComponent implements OnInit {
 
+export class ProductmasterComponent implements OnInit {
+  a:any=[];
   constructor(private dialog:MatDialog,private http:HttpClient) { }
   popup(){
     this.dialog.open(DialogproductComponent)
   }
-  a:any=[]
-  b:string[]=[]
+  
   showtable(){
     console.log("HI")
     this.http.get("http://localhost:3000/showtables").subscribe((data)=>{
       var key=Object.values(data)
-      var l=key.length;        
+      var l=key.length;
+      console.log(this.a)      
       for(let i=0;i<l;i++){
-        this.b=[]
-        this.b.push(key[i].productid)
-        this.b.push(key[i].productname)
-        this.a.push(this.b)
+        this.a.push({position:i,pid:key[i].productid,pname:key[i].productname})
       }
       console.log(this.a)
       // for(key in data){
