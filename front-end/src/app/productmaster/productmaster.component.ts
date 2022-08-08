d6a89e498944e8f93130d92dec63a2d497a89d32
unknown <santhoshyavd@gmail.com>
2022-08-08 22:45:54 +0530

    changes

--- front-end/src/app/productmaster/productmaster.component.ts
+++ front-end/src/app/productmaster/productmaster.component.ts
@@ -1,13 +1,8 @@
-
-
-
+import { HomeheaderComponent } from '../components/homeheader/homeheader.component';
 import {Component} from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { HttpClient } from '@angular/common/http';
 import { DialogproductComponent } from '../dialogproduct/dialogproduct.component';
-
-
-
 
 /**
  * @title Basic use of `<table mat-table>`
@@ -22,30 +17,16 @@ export class  ProductmasterComponent {
   constructor(private dialog:MatDialog,private http:HttpClient) { }
   popup(){
     this.dialog.open(DialogproductComponent)
-  // this.http.get("http://localhost:3000/showtables").subscribe((data)=>{
-  //     var key=Object.values(data)
-  //     var l=key.length;
-  //     for(let i=0;i<l;i++){
-  //       ELEMENT_DATA.push(key[i])
-  //       console.log(key[i])
-  //       // this.a.push({position:i+1,pid:key[i].productid,pname:key[i].productname})
-  //     }
-  //     console.log(ELEMENT_DATA)
-  // })
 }
 displayedColumns: string[] = ["pid","pname"];
 dataSource = ELEMENT_DATA;
 ngOnInit(): void {
-  console.log("Loads")
   this.http.get("http://localhost:3000/showtables").subscribe((data)=>{
       var key=Object.values(data)
       var l=key.length;
       for(let i=0;i<l;i++){
         ELEMENT_DATA.push(key[i])
-        console.log(key[i])
-        // this.a.push({position:i+1,pid:key[i].productid,pname:key[i].productname})
       }
-      console.log(ELEMENT_DATA)
   })
 }
 }
